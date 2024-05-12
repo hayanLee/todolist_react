d58e62dfce74bf8926e46d133ae276b7d299235a
hayanLee <hayanlee.dev@gmail.com>
2024-05-13 00:23:18 +0900

    update : Todo 삭제, 변경 기능 구현

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-export default function Todo({ todo }) {
-    const { title, content } = todo;
+export default function Todo({ todo, onDelete, onChange }) {
+    const { title, content, done } = todo;
+    const handleDelete = () => onDelete(todo);
+    const handleChange = () => onChange(todo);
+
     return (
         <div className='flex'>
             {title} : {content}
-            <button className='bg-slate-200 mr-2'>삭제</button>
-            <button className='bg-slate-200'>변경</button>
+            <button className='bg-slate-200 mr-2' onClick={handleDelete}>
+                삭제
+            </button>
+            <button className='bg-slate-200' onClick={handleChange}>
+                {done ? '취소' : '완료'}
+            </button>
         </div>
     );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,16 +4,30 @@ import Todo from './Todo';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
-        { title: '밥먹기', id: 1, content: '한식먹기' },
-        { title: '쇼핑', id: 2, content: '장보기' },
-        { title: '청소', id: 3, content: '대청소' },
+        { title: '밥먹기', id: 1, content: '한식먹기', done: true },
+        { title: '쇼핑', id: 2, content: '장보기', done: true },
+        { title: '청소', id: 3, content: '대청소', done: false },
     ]);
-    const handleAddTodo = (todo) => setTodos((prev) => [...prev, todo]);
+    const handleAddTodo = (todo) => setTodos([...todos, todo]);
+    const handleDeleteTodo = (todo) => setTodos(todos.filter((t) => t.id !== todo.id));
+    const handleChangeTodo = (todo) =>
+        setTodos(
+            todos.map((t) => {
+                if (t.id === todo.id) {
+                    return { ...t, done: !t.done };
+                } else return t;
+            })
+        );
     return (
         <div>
             <AddTodo onAdd={handleAddTodo} />
             {todos.map((todo) => (
-                <Todo key={todo.id} todo={todo} />
+                <Todo
+                    key={todo.id}
+                    todo={todo}
+                    onDelete={handleDeleteTodo}
+                    onChange={handleChangeTodo}
+                />
             ))}
         </div>
     );
