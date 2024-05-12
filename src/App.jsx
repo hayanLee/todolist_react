bc0a7f38b25f4d336af859a6cd83485cfb164136
hayanLee <hayanlee.dev@gmail.com>
2024-05-12 18:16:57 +0900

    script : tailwindcss 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function App() {
-    return <div>App</div>;
+    return (
+        <div>
+            <p className='text-8xl text-blue-500'>Hello</p>
+        </div>
+    );
 }
