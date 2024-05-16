77b18968858a89328ac4b14f2aad30a798d40a11
hayanLee <hayanlee.dev@gmail.com>
2024-05-16 17:12:43 +0900

    refactor : 수업 듣고 리팩토링
    - App.jsx에서 AddTodo, TodoList로 데이터 내려줌
    - 웹 페이지 title 수정
    - TodoList에 App.jsx에서 필터링 된 todos 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddTodo from './components/AddTodo';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
 
 export default function App() {
+    const [todos, setTodos] = useState([
+        { title: '밥먹기', id: 1, content: '맛점하기', isDone: false },
+        { title: '장보기', id: 2, content: '아이스크림 3개', isDone: false },
+        { title: '청소', id: 3, content: '대청소', isDone: false },
+        { title: '분리수거', id: 4, content: '페트병구기기', isDone: true },
+        {
+            title: '타이틀 최대 15글자',
+            id: 5,
+            content: '내용 최대 20글자',
+            isDone: false,
+        },
+    ]);
+    const handleAddTodo = (newTodo) => setTodos((prevTodos) => [...prevTodos, newTodo]);
+    const handleDeleteTodo = (deletedTodo) =>
+        setTodos((prevTodos) => prevTodos.filter((t) => t.id !== deletedTodo.id));
+    const handleChangeTodo = (changedTodo) =>
+        setTodos((prevTodos) =>
+            prevTodos.map((t) =>
+                t.id === changedTodo.id ? { ...t, isDone: !t.isDone } : t
+            )
+        );
+
+    const workingTodos = todos.filter((todo) => !todo.isDone);
+    const doneTodos = todos.filter((todo) => todo.isDone);
+
     return (
         <div className='w-screen h-screen bg-slate-100 flex justify-center overflow-hidden'>
             <div className='max-w-[1200px] min-w-[800px] bg-blue-500'>
                 <Header />
-                <TodoList />
+                <AddTodo onAdd={handleAddTodo} />
+                <TodoList
+                    title='Working 🔥'
+                    todos={workingTodos}
+                    onChange={handleChangeTodo}
+                    onDelete={handleDeleteTodo}
+                />
+                <TodoList
+                    title='Done 👍'
+                    todos={doneTodos}
+                    onChange={handleChangeTodo}
+                    onDelete={handleDeleteTodo}
+                />
             </div>
         </div>
     );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,70 +1,20 @@
-import React, { Fragment, useState } from 'react';
-import AddTodo from './AddTodo';
+import React from 'react';
 import Todo from './Todo';
 
-export default function TodoList() {
-    const [todos, setTodos] = useState([
-        { title: '밥먹기', id: 1, content: '맛점하기', isDone: false },
-        { title: '장보기', id: 2, content: '아이스크림 3개', isDone: false },
-        { title: '청소', id: 3, content: '대청소', isDone: false },
-        { title: '분리수거', id: 4, content: '페트병구기기', isDone: true },
-        {
-            title: '타이틀 최대 15글자',
-            id: 5,
-            content: '내용 최대 20글자',
-            isDone: false,
-        },
-    ]);
-    const handleAddTodo = (todo) => setTodos([...todos, todo]);
-    const handleDeleteTodo = (todo) => setTodos(todos.filter((t) => t.id !== todo.id));
-    const handleChangeTodo = (todo) =>
-        setTodos(
-            todos.map((t) => {
-                if (t.id === todo.id) {
-                    return { ...t, isDone: !t.isDone };
-                } else return t;
-            })
-        );
+export default function TodoList({ title, todos, onChange, onDelete }) {
     return (
-        <>
-            <AddTodo onAdd={handleAddTodo} />
-            <Fragment>
-                <div className='h-3/6 px-[20px] overflow-y-auto'>
-                    <h1 className='font-bold text-3xl my-[10px] text-white'>
-                        Working 🔥
-                    </h1>
-                    <div className='flex flex-wrap gap-[20px]'>
-                        {todos.map(
-                            (todo) =>
-                                !todo.isDone && (
-                                    <Todo
-                                        key={todo.id}
-                                        todo={todo}
-                                        onDelete={handleDeleteTodo}
-                                        onChange={handleChangeTodo}
-                                    />
-                                )
-                        )}
-                    </div>
-                </div>
-
-                <div className='h-3/6 px-[20px] overflow-y-auto'>
-                    <h1 className='font-bold text-3xl my-[10px] text-white'>isDone 👍</h1>
-                    <div className='flex flex-wrap gap-[20px]'>
-                        {todos.map(
-                            (todo) =>
-                                todo.isDone && (
-                                    <Todo
-                                        key={todo.id}
-                                        todo={todo}
-                                        onDelete={handleDeleteTodo}
-                                        onChange={handleChangeTodo}
-                                    />
-                                )
-                        )}
-                    </div>
-                </div>
-            </Fragment>
-        </>
+        <div className='h-3/6 px-[20px] overflow-y-auto'>
+            <h1 className='font-bold text-3xl my-[10px] text-white'>{title}</h1>
+            <div className='flex flex-wrap gap-[20px]'>
+                {todos.map((todo) => (
+                    <Todo
+                        key={todo.id}
+                        todo={todo}
+                        onDelete={onDelete}
+                        onChange={onChange}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
